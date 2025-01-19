dd6dc5e403179868df25eaf164fb5b65c26d66c3
Jitesh Kumar dewangan <jiteshdewangan99@gmail.com>
2025-01-19 16:05:56 +0530

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
 from sklearn.linear_model import LogisticRegression  # Example model
 from sklearn.datasets import make_classification       # Example dataset
 import numpy as np
 
-
-# In[2]:
-
-
 X , y =make_classification(n_samples=1000, n_features=20, random_state=42)
-
-
-# In[19]:
-
-
 X.shape
-
-
-# In[4]:
-
-
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 model = LogisticRegression()
-
 scores = cross_val_score(model, X, y, cv=kf, scoring='accuracy')
-
-
-
-# In[5]:
-
 
 print("K-Fold Cross-Validation Scores:", scores)
 print("Mean Accuracy:", np.mean(scores))
 print("Standard Deviation of Accuracy:", np.std(scores))
-
-
-# In[26]:
-
-
-## manual implementaion of Kfold
 
 def manual_kflod(X, y, k =5):
     fold_size = len(X)//5
@@ -68,16 +38,5 @@ def manual_kflod(X, y, k =5):
     
     return scores
 
-
-# In[27]:
-
-
 scores_manual = manual_kflod(X, y)
 print("Manual score : ", scores_manual)
-
-
-# In[ ]:
-
-
-
-
